3e5eebaf6fb86d019431c58d4251e3eaf0a2f026
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-06-21 16:40:08 +0200

    Created basic login form

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = {};
+
+    if (!email) {
+      errors.email = "Email is required";
+    }
+    if (!password) {
+      errors.password = "Password is required";
+    }
+    if (Object.keys(errors).length < 0) {
+      console.log("Login Confirmed");
+    } else {
+      setErrors(errors);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter email here"
+            onChange={handleEmailChange}
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter password here"
+            onChange={handlePasswordChange}
+          />
+          {errors.password && <span className="error">{errors.password}</span>}
+        </label>
+        <button type="Submit">Login</button>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="App">Welcome to Stack-Overflow</div>;
+  return <div>Welcome to Stack-Overflow</div>;
 };
 export default Home;
